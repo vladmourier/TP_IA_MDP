049dfa624507c1cb73b0cd1e72d15c60eacf6f42
Adrien <Adrien@134.214.118.239>
2016-03-14 15:39:04 +0100

    Adrien

--- src/agent/planningagent/ValueIterationAgent.java
+++ src/agent/planningagent/ValueIterationAgent.java
@@ -2,16 +2,11 @@ package agent.planningagent;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 
 import environnement.Action;
 import environnement.Etat;
 import environnement.MDP;
-import environnement.gridworld.ActionGridworld;
-import java.util.HashMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Cet agent met a jour sa fonction de valeur avec value iteration et choisit
@@ -26,7 +21,7 @@ public class ValueIterationAgent extends PlanningValueAgent {
      * discount facteur
      */
     protected double gamma;
-	//*** VOTRE CODE
+    //*** VOTRE CODE
 
     /**
      *
@@ -36,7 +31,7 @@ public class ValueIterationAgent extends PlanningValueAgent {
     public ValueIterationAgent(double gamma, MDP mdp) {
         super(mdp);
         this.gamma = gamma;
-		//*** VOTRE CODE
+        //*** VOTRE CODE
 
     }
 
@@ -51,13 +46,14 @@ public class ValueIterationAgent extends PlanningValueAgent {
      */
     @Override
     public void updateV() {
-		//delta est utilise pour detecter la convergence de l'algorithme
+        //delta est utilise pour detecter la convergence de l'algorithme
         //lorsque l'on planifie jusqu'a convergence, on arrete les iterations lorsque
         //delta < epsilon 
         this.delta = 0.0;
-		//*** VOTRE CODE
+        //*** VOTRE CODE
 
-		// mise a jour vmax et vmin pour affichage du gradient de couleur:
+        
+        // mise a jour vmax et vmin pour affichage du gradient de couleur:
         //vmax est la valeur de max pour tout s de V
         //vmin est la valeur de min pour tout s de V
         // ...
@@ -77,11 +73,7 @@ public class ValueIterationAgent extends PlanningValueAgent {
 
     @Override
     public double getValeur(Etat _e) {
-		//*** VOTRE CODE
-        
-        
-
-        return 0.0;
+        return ;
     }
 
     /**
@@ -92,17 +84,10 @@ public class ValueIterationAgent extends PlanningValueAgent {
     @Override
     public List<Action> getPolitique(Etat _e) {
         List<Action> l = new ArrayList<Action>();
-	l = this.getMdp().getActionsPossibles(_e);
-        double[] V = null;
-        int i = 0;
+        l = this.getMdp().getActionsPossibles(_e);
         
-        for (Action a : l)
-        {
-            V[i] = this.getValeur(this.getMdp().getEtatsAccessibles().get(i));
-        }
 
         //renvoie le argmax
-        
         return l;
 
     }
@@ -110,9 +95,7 @@ public class ValueIterationAgent extends PlanningValueAgent {
     @Override
     public void reset() {
         super.reset();
-		//*** VOTRE CODE
-
-        /*-----------------*/
+        Values = null;
         this.notifyObs();
 
     }
@@ -121,128 +104,4 @@ public class ValueIterationAgent extends PlanningValueAgent {
     public void setGamma(double arg0) {
         this.gamma = arg0;
     }
-
-public class ValueIterationAgent extends PlanningValueAgent{
-    /**
-     * discount facteur
-     */
-    protected double gamma;
-    private Map<Etat, Double> visited;
-    //*** VOTRE CODE
-    
-    
-    
-    /**
-     *
-     * @param gamma
-     * @param mdp
-     */
-    public ValueIterationAgent(double gamma,MDP mdp) {
-        super(mdp);
-        this.gamma = gamma;
-        //*** VOTRE CODE
-        visited = new HashMap<>();
-    }
-    
-    
-    public ValueIterationAgent(MDP mdp) {
-        this(0.9,mdp);
-        
-    }
-    
-    /**
-     *
-     * Mise a jour de V: effectue UNE iteration de value iteration
-     */
-    @Override
-    public void updateV(){
-        //delta est utilise pour detecter la convergence de l'algorithme
-        //lorsque l'on planifie jusqu'a convergence, on arrete les iterations lorsque
-        //delta < epsilon
-        this.delta=0.0;
-        //*** VOTRE CODE
-        
-        
-        
-        
-        // mise a jour vmax et vmin pour affichage du gradient de couleur:
-        //vmax est la valeur de max pour tout s de V
-        //vmin est la valeur de min pour tout s de V
-        // ...
-        
-        //******************* a laisser a la fin de la methode
-        this.notifyObs();
-    }
-    
-    
-    /**
-     * renvoi l'action executee par l'agent dans l'etat e
-     */
-    @Override
-    public Action getAction(Etat e) {
-        //*** VOTRE CODE
-        
-        
-        return this.getMdp().getActionsPossibles(e).get(new Random().nextInt(this.getMdp().getActionsPossibles(e).size()));
-    }
-    @Override
-    public double getValeur(Etat _e) {
-        //*** VOTRE CODE
-        double res = 0, proba, recompense, max = 0;
-        if(!visited.containsKey(_e)){
-            visited.put(_e, null);
-            for(Action a : this.getMdp().getActionsPossibles(_e)){
-                try {
-                    Map<Etat, Double> probabilites = this.getMdp().getEtatTransitionProba(_e,a);
-                    for(Etat e : probabilites.keySet()){
-                        proba = probabilites.get(e);
-                        recompense = this.getMdp().getRecompense(_e, a, e);
-                        res += proba * (recompense + (gamma * this.getValeur(e)));
-                    }
-                } catch (Exception ex) {
-                    Logger.getLogger(AgentRandom.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                max = res>max? res : max;
-            }
-            visited.putIfAbsent(_e, max);
-            
-        }
-        
-        return max;
-    }
-    /**
-     * renvoi la (les) action(s) de plus forte(s) valeur(s) dans l'etat e
-     * (plusieurs actions sont renvoyees si valeurs identiques, liste vide si aucune action n'est possible)
-     */
-    @Override
-    public List<Action> getPolitique(Etat _e) {
-        List<Action> l = new ArrayList<>();
-        //*** VOTRE CODE
-        
-        
-        return l;
-        
-    }
-    
-    @Override
-    public void reset() {
-        super.reset();
-        //*** VOTRE CODE
-        
-        
-        
-        
-        
-        /*-----------------*/
-        this.notifyObs();
-        
-    }
-    
-    
-    @Override
-    public void setGamma(double arg0) {
-        this.gamma = arg0;
-    }
-    
-    
 }
