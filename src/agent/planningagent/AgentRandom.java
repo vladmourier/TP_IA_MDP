2efe9879f4ae682390ecfca1035e1d174dd88452
Adrien <Adrien@134.214.118.239>
2016-03-14 11:56:52 +0100

    IA commit Adrien

--- src/agent/planningagent/AgentRandom.java
+++ src/agent/planningagent/AgentRandom.java
@@ -1,6 +1,5 @@
 package agent.planningagent;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,56 +7,54 @@ import environnement.Action;
 import environnement.Etat;
 import environnement.MDP;
 import environnement.gridworld.ActionGridworld;
+import java.util.Random;
+
 /**
- * Cet agent choisit une action aleatoire parmi toutes les autorisees dans chaque etat
+ * Cet agent choisit une action aleatoire parmi toutes les autorisees dans
+ * chaque etat
+ *
  * @author lmatignon
  *
  */
-public class AgentRandom extends PlanningValueAgent{
-	
-	
-	public AgentRandom(MDP _m) {
-		super(_m);
-	}
+public class AgentRandom extends PlanningValueAgent {
 
-	@Override
-	public Action getAction(Etat e) {
-		//*** VOTRE CODE
-		
-		
-		
-		return null;
-		
-	}
+    public AgentRandom(MDP _m) {
+        super(_m);
+    }
 
-	
-	
-	@Override
-	public double getValeur(Etat _e) {
-		return 0.0;
-	}
+    @Override
+    public Action getAction(Etat e) {
+	//*** VOTRE CODE
+        //faire get politique puis random dans les états
+        
+        List<Action> la = this.getPolitique(e);
+        Random r = new Random();
+        return la.get(r.nextInt(la.size()));
 
-	
+    }
 
-	@Override
-	public List<Action> getPolitique(Etat _e) {
-		//*** VOTRE CODE
-		
-		return null;
-	}
+    @Override
+    public double getValeur(Etat _e) {
+        return 0.0;
+    }
 
-	@Override
-	public void updateV() {
-		System.out.println("l'agent random ne planifie pas");
-	}
+    @Override
+    public List<Action> getPolitique(Etat _e) {
+        //*** VOTRE CODE
+        //Retrouver la liste de tous les mouvements dispos sur la grille
 
-	@Override
-	public void setGamma(double parseDouble) {
+        return this.getMdp().getActionsPossibles(_e);
+    }
+
+    @Override
+    public void updateV() {
+        System.out.println("l'agent random ne planifie pas");
+    }
+
+    @Override
+    public void setGamma(double parseDouble) {
 		// TODO Auto-generated method stub
-		
-	}
 
-
-
+    }
 
 }
